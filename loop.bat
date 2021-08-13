f144e48565ab547329b4b78acdc208229dee23c3
NoobBugHunter <60887399+NoobBugHunter@users.noreply.github.com>
2021-08-14 04:40:41 +0500

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -4,5 +4,5 @@ tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK t
 :check
 ping 127.0.0.1 > nul
 cls
-for /L %%a in (1,1,58) Do timeout /T 360 /NOBREAK > nul
+for /L %%a in (1,1,58) Do ping -n 360 127.0.0.1 >NUL > nul
 echo Finished
