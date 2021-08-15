37d90b83bc6d04d33332b775dc7b16e5d7f45331
NoobBugHunter <60887399+NoobBugHunter@users.noreply.github.com>
2021-08-15 15:03:40 +0500

    Delete loop.bat

--- loop.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Nob
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel," & ping 127.0.0.1 >Nul & exit
-:check
-ping 127.0.0.1 > nul
-cls
-for /L %%a in (1,1,58) Do ping -n 360 127.0.0.1 >NUL > nul
-echo Finished
